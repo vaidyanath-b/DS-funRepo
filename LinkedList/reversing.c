6070b2cc86cd33f6fe981d7136ea078ed0f76b0f
vaidyanath b <b.vaidyanath003@gmail.com>
2023-01-07 18:51:22 +0530

    2 methods

--- LinkedList/reversing.c
+++ LinkedList/reversing.c
@@ -8,32 +8,48 @@ typedef struct
     node **nodeItem; // pointer to an array of pointers
 } stack;
 
-void push(stack *s, node *current)
-{
-    if (s->top != SIZE - 1)
-        s->nodeItem[++s->top] = current;
-}
-
 void reverseList(linkedList *li)
 {
-    node *temp = li->head;
-    stack s;
-    s.top = -1;
-    s.nodeItem = (node **)malloc(SIZE * sizeof(node *));
-    while (temp->next != NULL)
+    node *temp = NULL;
+    node *temp1 = li->head;
+    node *temp2;
+    while (temp1->next != NULL)
     {
-        push(&s, temp);
-        temp = temp->next;
+        temp2 = temp1->next;
+        temp1->next = temp;
+        temp = temp1;
+        temp1 = temp2;
     }
-    push(&s, temp);
-    int t = s.top;
-    li->head = s.nodeItem[s.top];
-    while (t != 0)
-        s.nodeItem[t--]->next = s.nodeItem[t - 1];
-    s.nodeItem[t]->next = NULL;
-
-    free(s.nodeItem);
+    temp1->next = temp;
+    li->head = temp1;
 }
+
+// void push(stack *s, node *current)   // -Method2
+// {
+//     if (s->top != SIZE - 1)
+//         s->nodeItem[++s->top] = current;
+// }
+
+// void reverseList(linkedList *li)
+// {
+//     node *temp = li->head;
+//     stack s;
+//     s.top = -1;
+//     s.nodeItem = (node **)malloc(SIZE * sizeof(node *));
+//     while (temp->next != NULL)
+//     {
+//         push(&s, temp);
+//         temp = temp->next;
+//     }
+//     push(&s, temp);
+//     int t = s.top;
+//     li->head = s.nodeItem[s.top];
+//     while (t != 0)
+//         s.nodeItem[t--]->next = s.nodeItem[t - 1];
+//     s.nodeItem[t]->next = NULL;
+
+//     free(s.nodeItem);
+// }
 
 void main()
 {
